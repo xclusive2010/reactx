542adc907cff51a2abc2b41b7efd14dcc25e2f38
Michael <mchimaobi2010@gmail.com>
2023-03-10 10:26:04 +0100

    progerss

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,6 +11,9 @@ const Navbar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo"/>
         </div>
+        <div className='gpt3__navbar-links_container'>
+          
+        </div>
       </div>
     </div>
   )
